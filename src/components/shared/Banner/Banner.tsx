2128675d14b4d40026119146b2eee8bb4e38bb61
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2025-01-05 19:57:03 +0600

    add: starter pack

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
       <div className="container h-full flex justify-center items-center">
         <div className="w-[60%] h-[60%] rounded-xl text-center bg-white px-16 py-20">
           <p className="w-40 mx-auto rounded-full p-2 my-5 bg-teal-100 text-teal-600">
-            29 January 2024
+            1st January 2025
           </p>
           <h1 className="text-4xl">The Rise of Quantum Computing</h1>
           <p className="text-gray-400 mt-3 w-3/4 mx-auto">
